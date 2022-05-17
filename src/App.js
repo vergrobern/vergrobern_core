25bd905230a66a847c2e11387c4b6b87b697a2d1
mremmalex <mremmalex2@gmail.com>
2022-05-17 23:24:13 +0100

    fixed the routing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,28 @@
-import React from 'react';
-import Home from './components/Home';
-import {BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css'
-import About from './components/About';
-import Footer from './components/Footer';
-import Services from './components/Services';
-import Products from './components/Products';
-import Contact from './components/Contact';
-
-
+import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
 
 function App() {
-
-  return(
-    <Router>    
-      <div>
-      
-          <Route path='/' exact  component={Home} />
-     
-          
-          <Route path='/about' component={About} />
-
-          
-          <Route path='/services' component={Services} />
-
-
-          <Route path='/products' component={Products} />
-   
-
-          <Route path='/contact' component={Contact} />
-
-
-          <Footer />
-      </div>
-    </Router>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/services" element={<Services />} />
+					<Route path="/products" element={<Products />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer />
+		</>
+	);
 }
 
-export default App
+export default App;
